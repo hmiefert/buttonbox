4d5769e03e114ad89906148e92ef0d0f81ba5815
Holger Miefert <holger.miefert@gmail.com>
2023-02-19 14:52:21 +0100

    Added rotary encoders

--- boot.py
+++ boot.py
@@ -14,36 +14,19 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x75, 0x01,  #   Report Size (1)
     0x95, 0x20,  #   Report Count (32)
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
-    0x15, 0x81,  #   Logical Minimum (-127)
-    0x25, 0x7F,  #   Logical Maximum (127)
-    0x09, 0x30,  #   Usage (X)
-    0x09, 0x31,  #   Usage (Y)
-    0x75, 0x08,  #   Report Size (8)
-    0x95, 0x02,  #   Report Count (4)
-    0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x39,	 # USAGE (Hat switch) */
-    0x15, 0x01,	 # LOGICAL_MINIMUM (1) */
-    0x25, 0x08,	 # LOGICAL_MAXIMUM (8) */
-    0x95, 0x02,	 # REPORT_COUNT (2) */
-    0x75, 0x04,	 # REPORT_SIZE (4) */
-    0x25, 0x01,	 #  INPUT (Data,Var,Abs) */
     0xC0,        # End Collection
 ))
-
 
 gamepad = usb_hid.Device(
     report_descriptor=GAMEPAD_REPORT_DESCRIPTOR,
     usage_page=0x01,           # Generic Desktop Control
     usage=0x05,                # Gamepad
     report_ids=(4,),           # Descriptor uses report ID 4.
-    in_report_lengths=(6,),    # This gamepad sends 6 bytes in its report.
+    in_report_lengths=(8,),    # This gamepad sends 8 bytes in its report.
     out_report_lengths=(0,),   # It does not receive any reports.
 )
 
 usb_hid.enable(
     (usb_hid.Device.KEYBOARD,
-     usb_hid.Device.MOUSE,
-     usb_hid.Device.CONSUMER_CONTROL,
      gamepad)
 )
--- code.py
+++ code.py
@@ -1,58 +1,38 @@
 #import board support libraries, including HID.
 import board
 import digitalio
-import analogio
+import rotaryio
 import usb_hid
-
-from time import sleep
-
-#Libraries for communicating as a Keyboard device
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
 
 #library for communicating as a gamepad
 from hid_gamepad import Gamepad
 
-from adafruit_hid.mouse import Mouse
-mouse = Mouse(usb_hid.devices)
-
-from adafruit_hid.consumer_control_code import ConsumerControlCode
-from adafruit_hid.consumer_control import ConsumerControl
-
-mediacontrol = ConsumerControl(usb_hid.devices)
-
-keyboard = Keyboard(usb_hid.devices)
-layout = KeyboardLayoutUS(keyboard)
 gp = Gamepad(usb_hid.devices)
 
+enc1 = rotaryio.IncrementalEncoder(board.GP0, board.GP1)
+enc2 = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
+enc3 = rotaryio.IncrementalEncoder(board.GP6, board.GP7)
+
+enc1_last_pos = None
+enc2_last_pos = None
+enc3_last_pos = None
 #Create a collection of GPIO pins that represent the buttons
 #This includes the digital pins for the Directional Pad.
-#They can be used as regular buttons if using the analog inputs instead
-button_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP19, board.GP16, board.GP20, board.GP17, board.GP21, board.GP18, board.GP10, board.GP11, board.GP12, board.GP13,board.GP14, board.GP15)
+#They can be used as regular buttons if using the analog inputs instead )
+button_pins = (board.GP11, board.GP10,
+               board.GP12, board.GP13,
+               board.GP14, board.GP15,
+               board.GP19, board.GP20, board.GP21,
+               board.GP16, board.GP17, board.GP18,
+               board.GP2, board.GP5, board.GP8,
+               board.GP22, board.GP23,
+               board.GP24, board.GP25,
+               board.GP26, board.GP27)
 
 # Map the buttons to button numbers on the Gamepad.
 # gamepad_buttons[i] will send that button number when buttons[i]
 # is pushed.
-gamepad_buttons = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
-
-#Keyboard Mode Button Definitions
-keyboard_buttons = {0 : Keycode.UP_ARROW, 1 : Keycode.LEFT_ARROW, 2 : Keycode.DOWN_ARROW, 3 : Keycode.RIGHT_ARROW,
-                  4 : Keycode.LEFT_CONTROL, 5 : Keycode.SPACE, 6 : Keycode.W, 7 : Keycode.ENTER, 8 : Keycode.LEFT_ALT
-                    , 9 : Keycode.ENTER, 10 : Keycode.ENTER, 11 : Keycode.ENTER, 12 : Keycode.ENTER, 13 : Keycode.ENTER
-                    , 14 : Keycode.ENTER, 15 : Keycode.ENTER}
-
-#FPS Mode Button Definitions
-fps_buttons = {0 : Keycode.W, 1 : Keycode.A, 2 : Keycode.S, 3 : Keycode.D,
-                  4 : Keycode.LEFT_CONTROL, 5 : Keycode.SPACE, 6 : Keycode.LEFT_ALT, 7 : Keycode.ENTER,
-               8 : Keycode.ENTER, 9 : Keycode.ENTER, 10 : Keycode.ENTER, 11 : Keycode.ENTER, 12 : Keycode.ENTER
-               , 13 : Keycode.ENTER, 14 : Keycode.ENTER, 15 : Keycode.ENTER}
-
-#List of defind mode names
-mode_names = {1 : 'Gamepad', 2 : 'Keyboard', 3 : 'FPS', 4 : "Mouse", 5 : "Multimedia"}
-
-#Set Default Mode To 1
-mode = 1
+gamepad_buttons = (1, 2, 3 ,4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
 
 buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
 
@@ -60,43 +40,31 @@ buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
 for button in buttons:
     button.direction = digitalio.Direction.INPUT
     button.pull = digitalio.Pull.UP
-    
-# Setup for Analog Joystick as X and Y
-ax = analogio.AnalogIn(board.GP26)
-ay = analogio.AnalogIn(board.GP27)
-
-# Equivalent of Arduino's map() function.
-def range_map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
-  
-def debounce():
-    sleep(0.2)
-
+      
 while True:
-    setx = 0
-    sety = 0
-    #Not keyboard presses for directional
-    #So check them seperately first
-    if not buttons[0].value:
-        sety = -127
-    if not buttons[2].value:
-        sety = 127
-    if not buttons[1].value:
-        setx = -127
-    if not buttons[3].value:
-        setx = 127
-    #Set Joystick movements
-    gp.move_joysticks(
-        x=setx,
-        y=sety,
-    )
-        
-    # Go through all the button definitions, and
-    # press or release as appropriate
-    for i, button in enumerate(buttons):
-        if i > 3: #Skip the first 4, since they're the directionals
-            gamepad_button_num = gamepad_buttons[i - 4] # Minus 4 to ignore directionals
-            if button.value:
-                gp.release_buttons(gamepad_button_num)
-            else:
-                gp.press_buttons(gamepad_button_num)
+    pos1 = enc1.position
+    if enc1_last_pos != None and pos1 > enc1_last_pos:
+        gp.click_buttons(17)
+    elif enc1_last_pos != None and pos1 < enc1_last_pos:
+        gp.click_buttons(16)
+    enc1_last_pos = pos1
+ 
+    pos2 = enc2.position
+    if enc2_last_pos != None and pos2 > enc2_last_pos:
+        gp.click_buttons(19)
+    elif enc2_last_pos != None and pos2 < enc2_last_pos:
+        gp.click_buttons(18)
+    enc2_last_pos = pos2
+ 
+    pos3 = enc3.position
+    if enc3_last_pos != None and pos3 > enc3_last_pos:
+        gp.click_buttons(21)
+    elif enc3_last_pos != None and pos3 < enc3_last_pos:
+        gp.click_buttons(20)
+    enc3_last_pos = pos3
+ 
+    for btn_id, button in enumerate(buttons):
+        if button.value:
+            gp.release_buttons(btn_id+1)
+        else:
+            gp.press_buttons(btn_id+1)
